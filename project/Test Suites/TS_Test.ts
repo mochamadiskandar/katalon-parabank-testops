a8cb06cb909d444b02946f6a026bcacc478a8092
Mochamad Iskandar <mochamadiskandar19@gmail.com>
2025-04-13 22:11:05 +0700

    update config for testops and create new tc

---
+++ project/Test Suites/TS_Test.ts
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>5f8a1ae0-e314-4fd4-b6d7-f842a2ae1075</testSuiteGuid>
+   <testCaseLink>
+      <guid>71c4752d-92a0-473f-a6f4-d363b71d40af</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_valid login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>8208f5e9-0217-4f54-9134-58739a1109e5</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>91733d04-1a72-4808-a1e9-73ad1d4ba726</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6817648f-914c-4427-99b0-bd20d208c47a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_invalid login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>f90efa77-e67f-4cd6-a950-a0e3928eef33</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>fce97041-f17f-461c-9782-a37b7e6a6181</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
