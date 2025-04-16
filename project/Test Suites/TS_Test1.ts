f581b985614484c5ded12c3f2b05a0785d145018
Mochamad Iskandar <mochamadiskandar19@gmail.com>
2025-04-16 22:49:58 +0700

    update ts

--- project/Test Suites/TS_Test1.ts
+++ project/Test Suites/TS_Test1.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>TS_Test</name>
+   <name>TS_Test1</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
